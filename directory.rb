ba3409529ab06eb304615b407f8259752216c39c
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-02-26 21:21:38 +0000

    Adding student count

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 puts "The students of Villains Academy"
 puts "_____________"
 puts "Dr. Hannibal Lecter"
@@ -11,6 +12,4 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-print "Overall, we have "
-print 11 
-puts "great students"
+puts "Overall, we have #{student_count} great students"
