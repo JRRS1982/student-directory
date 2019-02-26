395f531e19ec35d5ae8bc36a912dda410bfe350c
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-02-26 21:40:00 +0000

    Adding an each loop and adding an array

--- directory.rb
+++ directory.rb
@@ -1,15 +1,22 @@
-student_count = 11
+
+students = [
+  "Dr. Hannibal Lecter", 
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 puts "The students of Villains Academy"
 puts "_____________"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-puts "Overall, we have #{student_count} great students"
+
+students.each do |student|
+  puts student
+end
+
+puts "Overall, we have #{students.count} great students"
