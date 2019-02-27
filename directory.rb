87f944242f6f1afaf097c78a44d100c7dc878034
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-02-27 08:47:17 +0000

    Moving from array to hash and updating print statement

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 
 students = [
-  "Dr. Hannibal Lecter", 
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", :november}, 
+  {name: "Darth Vader", :november}, 
+  {name: "Nurse Ratched", :november}, 
+  {name: "Michael Corleone", :november}, 
+  {name: "Alex DeLarge", :november}, 
+  {name: "The Wicked Witch of the West", :november}, 
+  {name: "Terminator", :november}, 
+  {name: "Freddy Krueger", :november}, 
+  {name: "The Joker", :november}, 
+  {name: "Joffrey Baratheon", :november}, 
+  {name: "Norman Bates", :november}
 ]
 
 def print_header
@@ -18,9 +18,9 @@ def print_header
   puts "_____________"
 end
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
