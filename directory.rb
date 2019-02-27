dde2923c258fefe62d462cc0b29f81f01ee45032
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-02-27 11:30:07 +0000

    few updates and reverting to the original

--- directory.rb
+++ directory.rb
@@ -1,52 +1,65 @@
-def print_header
-  puts "The students of Villains Academy"
-  puts "_____________"
-end
-
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
+@students = [] # an empty array accessible to all methods
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students =[]
+  # get the first name
   name = gets.chomp
+  # while the name is not empty, repeat this code
   while !name.empty? do
-    students <<{name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    # add the student hash to the array
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    # get another name from the user
     name = gets.chomp
   end
-  students
 end
 
 def interactive_menu
-  students =[]
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "3. Exit"
-    selection = gets.chomp
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit 
-    else
-      puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
-end 
+end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
 
 interactive_menu
