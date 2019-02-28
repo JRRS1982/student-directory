dc1aeaab3ccede7bad992705b649b7b25b22f899
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-02-28 12:19:47 +0000

    added ability to select all students with a specific initial

--- directory.rb
+++ directory.rb
@@ -27,7 +27,8 @@ def print_menu
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
-  puts "9. Exit" # 9 because we'll be adding more items
+  puts "5. Print list of students with selected initial"
+  puts "9. Exit"
 end
 
 def show_students
@@ -46,10 +47,24 @@ def process(selection)
       save_students
     when "4"
       load_students
+    when "5"
+      select_students_initial
     when "9"
       exit # this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
+  end
+end
+
+def select_students_initial
+  var = []
+  filtered = []
+  puts "Enter a first initial you'd like to see"
+  var = gets.chomp
+  @students.each do |x|
+    if x[:name].chars.first == var
+      puts "#{x[:name]} (#{x[:cohort]} cohort)"
+    end 
   end
 end
 
@@ -59,8 +74,9 @@ def print_header
 end
 
 def print_student_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  @students.each_with_index do |student, index|
+    index += 1
+    puts "#{index}. " "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -72,7 +88,7 @@ def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
-      @students << {name: name, cohort.to_sym}
+      @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
@@ -88,14 +104,15 @@ def save_students
 end
 
 def try_load_students
-  filename = ARGV.first
-  return if filename.nil?
-  if File.exists?(filename)
+  filename = ARGV.first# first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
     load_students(filename)
-      puts "Loaded #{@students.count} from #{filename}"
-  else 
-    puts "Sorry #{filename} doesn't exist."
-    exit 
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
 end
 
 interactive_menu
